fe6fcb370b03380aed172f02864a3d4f4d0ed0f5
fomik2 <fomin_v87@bk.ru>
2014-07-31 20:16:39 +0400

    Finish task 3

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,8 @@
 class CardsController < ApplicationController
   
   def new
-    #добавили, чтобы при отображении вьюхи new @cards не был nil 
-    #и не вылетало ошибки на if @cards.errors.any
+    # добавили, чтобы при отображении вьюхи new @cards не был nil 
+    # и не вылетало ошибки на if @cards.errors.any
     @card = Card.new 
   end                 
   
@@ -11,20 +11,20 @@ class CardsController < ApplicationController
   end
   
   def create
-   #создаем запись из тех параметров, которые разрешили передавать
+   # создаем запись из тех параметров, которые разрешили передавать
    @card = Card.new(card_params) 
-   #сохраняем запись в базу
+   # сохраняем запись в базу
    if @card.save 
-     #идем в представление show
+     # идем в представление show
      redirect_to @card 
    else
-    #если не сработало, то перегружаем представление
+    # если не сработало, то перегружаем представление
     render 'new' 
    end 
   end
 
   def show
-    #ищем в базе запись по id и выводим в представлении show
+    # ищем в базе запись по id и выводим в представлении show
     @card = Card.find(params[:id]) 
   end
 
@@ -48,7 +48,7 @@ class CardsController < ApplicationController
   end
 
 private
-  #определяем те параметры, которые можно передавать в метод контроллера create
+  # определяем те параметры, которые можно передавать в метод контроллера create
   def card_params 
     params.require(:card).permit(:original_text, :translated_text, :review_date)
   end 
