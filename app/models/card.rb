0d05e999fa5dc255fd73167da81b3b3df19ca795
fomik2 <fomin_v87@bk.ru>
2014-07-31 10:06:23 +0400

    Finish Task 3

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,3 @@
 class Card < ActiveRecord::Base
-  validates :original_text, presence: true
-  validates :translated_text, presence: true
-  validates :review_date, presence: true
+  validates :original_text, :translated_text, :review_date, presence: true
 end
