392223c55797bdd7a17e784be58434089254870c
fomik2 <fomin_v87@bk.ru>
2014-08-05 22:37:10 +0400

    Commit task_5_finish

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -6,11 +6,11 @@ describe Card do
     @test_card = Card.new(original_text: "dog", translated_text: "собака", review_date: "2014-08-03")
   end
   
-  it "Is check_translation method work right? Params in method shoud be eq with cell in database" do
+  it "does check 'check_translation' method work" do
     expect(@test_card.check_translation("собака")).to be true
   end
   
-  it "Is update_review_date method work right? Update rewie_data shoud pass" do
+  it "does check 'update_review_data' method work" do
     expect(@test_card).to respond_to(:update_review_date)
   end
 
