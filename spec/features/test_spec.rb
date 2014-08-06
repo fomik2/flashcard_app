fc27b211e80d91e66ed2e3d45ad559a23115405b
fomik2 <fomin_v87@bk.ru>
2014-08-06 23:20:12 +0400

    Commit task_6(2)

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -7,31 +7,26 @@ describe "Index Page" do
   end
 
   it "should have content 'Именно'" do
-  	visit '/'
-  	expect(page).to have_content('Именно')
+    visit '/'
+    expect(page).to have_content('Именно')
   end
 
   it "does test fail translate from user" do
-  	visit root_path
-  	fill_in 'translated_text', with: 'wqww'
-  	click_on 'Проверить'
-  	expect(page).to have_content('Неверно!')
+    visit root_path
+    fill_in 'translated_text', with: 'wqww'
+    click_on 'Проверить'
+    expect(page).to have_content('Неверно!')
   end
 
   it "does New page create a new object and add it to database" do
-  	visit new_card_path
-  	expect(page).to have_content('Add new card')
-  	fill_in 'card[original_text]', with: 'test'
-  	fill_in 'card[translated_text]', with: 'тест'
-  	select '2013', from: 'card_review_date_1i'
-  	select 'January', from: 'card_review_date_2i'
-  	select '12', from: 'card_review_date_3i'
-  	click_on 'Create Card'
-  	expect(page).to have_content('test')
+    visit new_card_path
+    expect(page).to have_content('Add new card')
+    fill_in 'card[original_text]', with: 'test'
+    fill_in 'card[translated_text]', with: 'тест'
+    select '2013', from: 'card_review_date_1i'
+    select 'January', from: 'card_review_date_2i'
+    select '12', from: 'card_review_date_3i'
+    click_on 'Create Card'
+    expect(page).to have_content('test')
   end
-
-  after(:each) do
-  	
-  end
-
 end
