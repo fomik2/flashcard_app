9e1321a44734b4d4a87491dc4533d700dd7ce945
fomik2 <fomin_v87@bk.ru>
2014-08-07 22:38:20 +0400

    Commit task_6(7)

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 describe "Index Page" do
 
-  it "shows error messages if translation is wrong" do
-    @card = FactoryGirl.create(:card)
+  it "check behavior then translation is wrong" do
+    FactoryGirl.create(:card)
     visit root_path
     fill_in 'translated_text', with: 'some text'
     click_on 'Проверить'
     expect(page).to have_content('Неверно!')
   end
 
-  it "check true translation" do
-    @card = FactoryGirl.create(:card)
+  it "check behavior then translation is true" do
+    FactoryGirl.create(:card)
     visit root_path
     fill_in 'translated_text', with: 'Микрософт'
     click_on 'Проверить'
     expect(page).to have_content('Непросмотренных карточек нет')
-
   end
 
   it "check page then object is not created" do
