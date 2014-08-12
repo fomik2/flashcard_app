a06c152609b927aa35eec4d195d05c556b1356de
fomik2 <fomin_v87@bk.ru>
2014-08-12 18:32:53 +0400

    Commit task7.2(7)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
 private
   def not_authenticated
-  	redirect_to login_path, alert: "Необходимо войти"
+    redirect_to login_path, alert: "Необходимо войти"
   end
 end
