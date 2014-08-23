8c4d3cad77f9053d8d891725529b92f555be1bf6
fomik2 <fomin_v87@bk.ru>
2014-08-23 22:50:00 +0400

    Edit user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
  describe User do
 
    before :each do
-     @user = User.new(email: "user@user.com", password: '12345', password_confirmation: '12345')
-     @current_category = 1
+     @user = User.create(email: "user@user.com", password: '12345', password_confirmation: '12345')
+     @category = Category.create(name: "Test", about: "Test category", user_id: @user.id) 
    end
 
    it "does check 'set_current_category' method work" do
-     expect { @user.set_current_category(@current_category) }.to change(@user, :current_category_id).to(1)
+     expect { @user.set_current_category(@category.id) }.to change(@user, :current_category_id).to(@category.id)
    end
 
  end
