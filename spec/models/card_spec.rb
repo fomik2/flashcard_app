8839b3f61210ea73a8cfe3142d15fcb71de58ac5
fomik2 <fomin_v87@bk.ru>
2014-08-27 17:17:07 +0400

    Edit card_spec

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -20,37 +20,58 @@ require 'rails_helper'
    end
 
     it "does check 'check_translation (review_date changer check)' method work" do
-     @card_for_check = Card.new(original_text: "dog", translated_text: "собака",
-       review_date: "2014-08-03", num_of_wrong: 2, num_of_right: 3)
-     expect{ @card_for_check.check_translation("собака") }.to change(
-       @card_for_check, :review_date).to(Date.today + 14)
+     @card_for_check = Card.new({ original_text: "dog",
+                                  translated_text: "собака",
+                                  review_date: "2014-08-03",
+                                  num_of_wrong: 2, 
+                                  num_of_right: 3 })
+     expect {
+       @card_for_check.check_translation("собака")
+     }.to change(@card_for_check, :review_date).to(Date.today + 14)
    end
 
    it "does check 'check_translation (num_of_right changer)' method work" do
-     @card_for_check = Card.new(original_text: "dog", translated_text: "собака",
-       review_date: "2014-08-03", num_of_wrong: 2, num_of_right: 4)
-     expect{ @card_for_check.check_translation("собака") }.to change(
-       @card_for_check, :num_of_right).to(5)
+     @card_for_check = Card.new({ original_text: "dog",
+                                 translated_text: "собака",
+                                 review_date: "2014-08-03",
+                                 num_of_wrong: 2,
+                                 num_of_right: 4})
+     expect {
+       @card_for_check.check_translation("собака")
+     }.to change(@card_for_check, :num_of_right).to(5)
    end
 
    it "does check 'increase_correct_answer_counter' method work" do
-     @card_for_check = Card.new(original_text: "dog", translated_text: "собака", 
-       review_date: "2014-08-03", num_of_wrong: 2, num_of_right: 6)
-     expect{ @card_for_check.increase_correct_answer_counter }.to change(@card_for_check, :review_date).to(Date.parse("2014-09-10"))
+     @card_for_check = Card.new({ original_text: "dog",
+                                  translated_text: "собака", 
+                                  review_date: "2014-08-03",
+                                  num_of_wrong: 2,
+                                  num_of_right: 6})
+     expect { 
+       @card_for_check.increase_correct_answer_counter 
+     }.to change(@card_for_check, :review_date).to(Date.parse("2014-09-10"))
    end
 
    it "does check 'increase_incorrect_answer_counter (change num_of_right)' method work" do
-     @card_for_check = Card.new(original_text: "dog", translated_text: "собака",
-       review_date: "2014-08-03", num_of_wrong: 3, num_of_right: 6)
-     expect{ @card_for_check.increase_incorrect_answer_counter }.to change(
-       @card_for_check, :num_of_right).to(1)
+     @card_for_check = Card.new({ original_text: "dog",
+                                  translated_text: "собака",
+                                  review_date: "2014-08-03",
+                                  num_of_wrong: 3, 
+                                  num_of_right: 6})
+     expect {
+       @card_for_check.increase_incorrect_answer_counter 
+     }.to change( @card_for_check, :num_of_right).to(1)
    end
 
    it "does check 'increase_incorrect_answer_counter (change_review_date)' method work" do
-     @card_for_check = Card.new(original_text: "dog", translated_text: "собака",
-       review_date: "2014-08-03", num_of_wrong: 3, num_of_right: 4)
-     expect{ @card_for_check.increase_incorrect_answer_counter }.to change(
-       @card_for_check, :review_date).to(Date.today.next_day)
+     @card_for_check = Card.new({ original_text: "dog",
+                                 translated_text: "собака",
+                                 review_date: "2014-08-03",
+                                 num_of_wrong: 3,
+                                 num_of_right: 4})
+     expect {
+       @card_for_check.increase_incorrect_answer_counter 
+     }.to change(@card_for_check, :review_date).to(Date.today.next_day)
    end
 
  end
