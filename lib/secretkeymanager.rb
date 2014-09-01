6ceae708a435a5c71475a6ab013a2b028d94fd65
fomik2 <fomin_v87@bk.ru>
2014-09-01 15:22:29 +0400

    edit secretkeymanager.rb

--- lib/secretkeymanager.rb
+++ lib/secretkeymanager.rb
@@ -1,7 +1,5 @@
 require 'yaml'
 module SecretKeyManager
-  CONFIG_PATH = File.join(Rails.root, 'config/aws.yml')
-  SOCNET_PATH = File.join(Rails.root, 'config/socnet.yml')
 
   def self.config(path)
     @_config ||= YAML.load(ERB.new(File.read(File.join(Rails.root, path))).result)[Rails.env]
