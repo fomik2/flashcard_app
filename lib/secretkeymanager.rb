24168dedca7c8d5958045ed01aa7d56eb859b3da
fomik2 <fomin_v87@bk.ru>
2014-09-01 16:30:31 +0400

    edit secretmanager

--- lib/secretkeymanager.rb
+++ lib/secretkeymanager.rb
@@ -2,7 +2,7 @@ require 'yaml'
 module SecretKeyManager
 
   def self.config(path)
-    @_config ||= YAML.load(ERB.new(File.read(File.join(Rails.root, path))).result)[Rails.env]
+    @_config ||= YAML.load(ERB.new(File.read(File.join(Rails.root, "config/#{ path }"))).result)[Rails.env]
   end
 
 end
