b0a9595522ea69b202dfc05b7ee36cb7caaf1b33
fomik2 <fomin_v87@bk.ru>
2014-09-04 21:06:44 +0400

    edit time.js

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,5 +1,5 @@
 var counter = 0;
 var timer = setInterval(function() 
             { $("#timer_value").val(++counter);
-              if (counter >= 60) {clearInterval(timer); }
+              if (counter >= 60) clearInterval(timer); 
             }, 1000);
