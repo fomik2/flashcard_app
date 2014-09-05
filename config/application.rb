2ba58a892279bb6597477eca9f05df8860466f41
fomik2 <fomin_v87@bk.ru>
2014-09-05 16:14:51 +0400

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module FlashcardApp
   class Application < Rails::Application
-    config.autoload_paths += %W( #{Rails.root}/app/services )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
